a0f7efe5cb7c799ca2a0c803944642d55e413a40
Phan Trinh <phan181299@gmail.com>
2019-11-09 23:46:41 -0500

    Moving training to mse for simplicity, fix typos

--- Pong.py
+++ Pong.py
@@ -1,8 +1,8 @@
 import gym
 import tensorflow as tf
 import tensorflow.keras as keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Convolution2D, Flatten
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Convolution2D, Flatten, Input, Multiply, Lambda
 import numpy as np
 import scipy
 from collections import deque
@@ -14,32 +14,21 @@ import time
 from gym import wrappers
 class Agent(object):
 
-    def __init__(self, env, actions, action_map, gamma=0.9, num_eps=10000, mem_len=2000):
+    def __init__(self, env, actions, action_map, gamma=0.99, num_eps=10000):
         self.env = env
         self.model = None
         self.trainer = None
         self.gamma = gamma
         self.num_eps = num_eps
-        self.mem = deque(maxlen=mem_len)
         self.actions = actions
         self.action_map = action_map
         self.__trained = False
         
     def m_loss(self, episode_reward):
         def loss(y_true,y_pred):
-            # feed in y_true as actual action taken 
-            # if actual action was up, we feed 1 as y_true and otherwise 0
-            # y_pred is the network output(probablity of taking up action)
-            # note that we dont feed y_pred to network. keras computes it
-
-            # first we clip y_pred between some values because log(0) and log(1) are undefined
-            tmp_pred = keras.layers.Lambda(lambda x: keras.backend.clip(x,0.05,0.95))(y_pred)
-            # we calculate log of probablity. y_pred is the probablity of taking up action
-            # note that y_true is 1 when we actually chose up, and 0 when we chose down
-            # this is probably similar to cross enthropy formula in keras, but here we write it manually to multiply it by the reward value
-            tmp_loss = keras.layers.Lambda(lambda x:-y_true*keras.backend.log(x)-(1-y_true)*(keras.backend.log(1-x)))(tmp_pred)
+            tmp_loss = Lambda(lambda x: keras.backend.mean(keras.backend.square(y_pred - y_true), axis=-1))(y_pred)
             # multiply log of policy by reward
-            policy_loss=keras.layers.Multiply()([tmp_loss,episode_reward])
+            policy_loss = Multiply()([tmp_loss,episode_reward])
             return policy_loss
         return loss
 
@@ -48,7 +37,7 @@ class Agent(object):
         Build a new model
         '''
         inputs = keras.layers.Input(shape=(80*80, ))
-        full_connect_1 = Dense(units=200,activation='relu',use_bias=False,)(flattened_layer)
+        full_connect_1 = Dense(units=200,activation='relu',use_bias=False,)(inputs)
         sigmoid_output = Dense(1,activation='sigmoid',use_bias=False)(full_connect_1)
         policy_network_model = keras.models.Model(inputs=inputs,outputs=sigmoid_output)
         policy_network_model.summary()
@@ -78,75 +67,38 @@ class Agent(object):
     def train(self, step_limit=2000, epsilon=0.9, epsilon_decay=0.95, batch_size=2):
         self._model()
         self.__trained = True
-        '''
-                memory <- empty
-				model <- untrained model
-				for each episode:
-					current-state = environment's initial state
-					processed-state = process-state(state)
-					for each step at playing the game or it timeouts:
-						action <- make epsilon-greedy decision based on the current state
-						get reward, next state, is_terminal from the actions on the environment
-						store this transition (s_{t}, action, s_{t + 1), is_terminal) into memory
-						batch <- sample a random batch (might consider batch size here)
-						rewards = empty
-						for each sample in the batch:
-							label each sample's reward with
-							reward if terminal state
-							reward = reward + gamma * model's prediction on next state
-							add reward to rewards
-						train the model on the batch's states and rewards
-        '''
         transitions = []
-        rewards_eps = []
+        actions = []
         rewards = []
-        for _ in range(self.num_eps):
+        for eps in range(self.num_eps):
+            running_epsilon = epsilon
+            rewards_ep = []
             pre_pview = None
             curr_view = self.env.reset()
             curr_pview = self._processed_frame(curr_view) #to be stored         
-            
-            for _ in range(step_limit):
+            reward_sum = 0
+            while True:
                 transition = curr_pview - pre_pview if pre_pview is not None else np.zeros_like(curr_pview)
-                action = self._epsilon_greedy(transition, epsilon)
-                rewards_eps.append(float(action))
+                action = self._epsilon_greedy(transition, running_epsilon)
+                actions.append(float(action))
                 transitions.append(transition)
                 next_view, reward, done, _ = self.env.step(self.action_map[action])
                 next_pview = self._processed_frame(next_view)
                 rewards.append(reward)
+                reward_sum += reward
                 pre_pview = curr_pview
                 curr_pview = next_pview
-#                 if len(self.mem) > batch_size:
-#                     pviews, rewards = self.training_data(batch_size)
-#                     loss = self.model.train_on_batch(pviews, rewards)
+                if reward != 0: # Pong has either +1 or -1 reward exactly when game ends.
+                  print('ep {}: match finished, reward: {}'.format(eps, reward) + ('' if reward == -1 else ' !!!!!!!!'))
                 if done:
                     print('Finished full game')
                     break
-#                 if epsilon >= 0.01:
-#                     epsilon *= epsilon_decay
             if len(transitions) % batch_size == 0:
               print(np.array(transitions).shape)
-              print(np.sum(rewards_eps))
-              self.train.fit([np.array(transitions), self.discount_reward(rewards, self.gamma)], np.array(rewards_eps))
-              transitions, rewards_eps, rewards = [], [], []
-            print('Finished one episode with total reward ', np.sum(rewards))
-                
-    def training_data(self, batch_size):
-        batch = random.sample(self.mem, batch_size)
-        pviews = []
-        rewards = []
-        for pview, reward, done, action, next_pview in batch:
-            pviews.append(pview)
-            next_pview = self._to_model_data(next_pview)
-            target = reward
-            if not done:
-                target = reward + self.gamma*np.amax(self.model.predict(next_pview)[0])
-            reward_model = self.model.predict(self._to_model_data(pview))
-            reward_model[0][action] = target
-            rewards.append(reward_model[0])
-        rewards_normed = np.array(rewards)
-#         rewards_normed -= np.mean(rewards)
-#         rewards_normed /= np.std(rewards)
-        return np.array(pviews), np.array(rewards)
+              print(np.sum(actions))
+              self.trainer.train_on_batch([np.array(transitions), self.discount_rewards(rewards, self.gamma)], np.array(actions))
+              transitions, actions, rewards = [], [], []
+            print('Finished one episode with total reward ', np.sum(reward_sum))
 
     def discount_rewards(self, r, gamma):
         """ take 1D float array of rewards and compute discounted reward """
@@ -193,16 +145,20 @@ class Agent(object):
 if __name__ == '__main__':
     env = gym.make('Pong-v0')
     view = env.reset()
-    agent = Agent(env, actions=2, action_map={1:2, 0:3}, num_eps=100)
+    agent = Agent(env, actions=2, action_map={1:2, 0:3}, num_eps=21)
     agent.train()
     env.close()
-    env = gym.make('Pong-v0')
     env = wrappers.Monitor(env, "./gym-results", force=True)
-    view = env.reset()
-    for _ in range(1000):
-        action = agent._epsilon_greedy(agent._processed_frame(view), 0.5)
+    pre_pview = None
+    curr_pview = agent._processed_frame(env.reset())
+    for _ in range(2000):
+        transition = curr_pview - pre_pview if pre_pview is not None else np.zeros_like(curr_pview)
+        action = agent.model.predict(np.expand_dims(transition, axis=0))
+        action = 1 if action > np.random.uniform() else 0
         observation, reward, done, info = env.step(agent.action_map[action])
         if done: break
+        curr_pview = agent._processed_frame(observation)
+        pre_pview = curr_pview
     env.close()
     
 
